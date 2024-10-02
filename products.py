cb13448b9e95458ac7bb29236dfff535e294ec97
jwsu1014 <jwsu1014@gmail.com>
2024-10-02 18:48:01 +0800

    refactor then  add main()

--- products.py
+++ products.py
@@ -1,21 +1,17 @@
-
+import os # operating system
 
 # 讀取檔案
 def read_file(filename):
-	import os # operating system
 	products = []
-	if os.path.isfile(filename): # 檢查檔案在不在
-		print('yeah! 找到檔案了!')
-		with open(filename, 'r', encoding = 'utf-8') as f:
-			for line in f:
-				if '商品,價格' in line:
-					continue # 繼續
-				name, price = line.strip().split(',')
-				products.append([name, price])
+	with open(filename, 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue # 繼續
+			name, price = line.strip().split(',')
+			products.append([name, price])
 		print(products)
-	else:
-		print('找不到檔案.....')
 	return products	
+
 # 讓使用者輸入
 def user_input(products):
 	while True:
@@ -47,8 +43,18 @@ def write_file(filename, products):
 		for p in products:
 			f.write(p[0] + ',' + str(p[1]) + '\n' )
 
-products = read_file('products.csv')
-products = user_input(products)
-print_products(products)
-write_file('products.csv', products)
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): # 檢查檔案在不在
+		print('yeah! 找到檔案了!')
+		products = read_file(filename)
+	else:
+		print('找不到檔案.....')
+		products = []
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
 
